09a2762dfd6f0eb5f7f9ec7e37806a02a4a67333
denys-bobrovnykov <62986552+denys-bobrovnykov@users.noreply.github.com>
2020-08-21 21:45:22 +0300

    First commit HW2 Task 1.

---
+++ homework2/app/main.js
@@ -0,0 +1,42 @@
+// --------- Task 1 --------------
+function taskOne(){
+    const dimentions = {
+        'length' : 0,
+        'width' : 0,
+        'height': 0
+    }
+    const paintCan = 16;
+    alert('TASK 1');
+    alert('We have an office and need to calculate a number of paint cans required to paint all walls.');
+    dimentions.length = +getInput('length');
+    console.log(dimentions.length);
+    dimentions.width = +getInput('width');
+    dimentions.height = +getInput('height');
+    const officeArea = (dimentions.length + dimentions.width) * 2 * dimentions.height;
+    console.log(officeArea);
+    const cansRequired = (officeArea % paintCan) ? ((officeArea - (officeArea % paintCan)) / paintCan + 1) : officeArea / paintCan;
+    alert(cansRequired);
+
+    // Functions for Task 1
+    function getInput(mesure){    // Takes a string
+        let check = false;
+        let userInput = '';
+        mesure = String(mesure).toUpperCase();
+        do{
+            userInput = prompt(`Please enter office ${mesure} in meters(Format: number not more than 1000).`);
+            check = !isNaN(+userInput) && +userInput <= 1000 && +userInput > 0 && userInput !== null;
+            if(!check && userInput !== null){
+                alert('Try again. Remember, only positive number not more than 1000!');
+            }else if (userInput === null){
+                alert('Sorry, you can not cancel until you enter a valid value.');
+            }
+        }while(!check); 
+        return userInput;   
+    }
+}   // End Functions
+// --------------------------------
+
+// --------- Task 2 ---------------
+
+
+
