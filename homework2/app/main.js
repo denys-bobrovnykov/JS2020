8af8d8a4d79febd25279aaa8b92054198ea5ad88
denys-bobrovnykov <62986552+denys-bobrovnykov@users.noreply.github.com>
2020-08-22 00:27:29 +0300

    HW.Task2 first commit.

--- homework2/app/main.js
+++ homework2/app/main.js
@@ -1,30 +1,33 @@
+'use strict';
 // --------- Task 1 --------------
 function taskOne(){
-    const dimentions = {
-        'length' : 0,
-        'width' : 0,
-        'height': 0
+    const dimensions = {
+        length : 0,
+        width : 0,
+        height: 0
     }
     const paintCan = 16;
     alert('TASK 1');
     alert('We have an office and need to calculate a number of paint cans required to paint all 4 =) walls.');
-    dimentions.length = +getInput('length');
-    dimentions.width = +getInput('width');
-    dimentions.height = +getInput('height');
-    const officeArea = ((dimentions.length + dimentions.width) * 2) * dimentions.height;
-    const cansRequired = (officeArea % paintCan) ? ((officeArea - (officeArea % paintCan)) / paintCan + 1) : officeArea / paintCan;
-    alert(cansRequired);
+    dimensions.length = +getInput('length');
+    dimensions.width = +getInput('width');
+    dimensions.height = +getInput('height');
+    const officeArea = ((dimensions.length + dimensions.width) * 2) * dimensions.height;
+    const cansRequired = (officeArea % paintCan) ? ((officeArea - (officeArea % paintCan)) / paintCan + 1) : (officeArea / paintCan);
+    alert(`Cans required: ${cansRequired}.`);
 
     // Functions for Task 1
-    function getInput(mesure){    // Takes a string
+    function getInput(mesure){      // Takes a string for text prompt
+        mesure = String(mesure).toUpperCase(); 
+        const maxValue = 1000;
+        const text = `Please enter office ${mesure} in meters(Format: number not more than ${maxValue}).`
         let check = false;
         let userInput = '';
-        mesure = String(mesure).toUpperCase();
         do{
-            userInput = prompt(`Please enter office ${mesure} in meters(Format: number not more than 1000).`);
-            check = !isNaN(+userInput) && +userInput <= 1000 && +userInput > 0 && userInput !== null;
+            userInput = prompt(text);
+            check = !isNaN(+userInput) && +userInput <= maxValue && +userInput > 0 && userInput !== null;
             if(!check && userInput !== null){
-                alert('Try again. Remember, only positive number not more than 1000!');
+                alert(`Try again. Remember, only positive number not more than ${maxValue}!`);
             }else if (userInput === null){
                 alert('Sorry, you can not cancel until you enter a valid value.');
             }
@@ -37,6 +40,48 @@ function taskOne(){
 // --------------------------------
 
 // --------- Task 2 ---------------
-
-
+function taskTwo(){
+    alert('TASK 2.');
+    alert('GOLD RUSH!');
+    let prices = [];
+    let buckets = [];
+    let result = 0;
+    // Get all inputs in lists
+    prices.push(+getInput('Gold A1 price'));
+    prices.push(+getInput('Gold A2 price'));
+    prices.push(+getInput('Gold A3 price'));
+    buckets.push(+getInput('Bucket B1 max volume in kg'));
+    buckets.push(+getInput('Bucket B2 max volume in kg'));
+    buckets.push(+getInput('Bucket B3 max volume in kg'));
+    // Sort all lists(by number descending) https://www.javascripttutorial.net/javascript-array-sort/
+    prices.sort((a,b) => b - a);
+    buckets.sort((a,b) => b - a);
+    // Put the most expensive gold in the biggest bucket...
+    for (let i = 0; i < prices.length; i++){
+        result += prices[i] * buckets[i];
+    }
+    alert(`Prices|${prices}| to Buckets|${buckets}|`);
+    alert(`Your max gold value is ${parseInt(result)} rubles.`);
+    
+    // Functions for Task 2
+    function getInput(mesure){    // Takes a string for text prompt
+        mesure = String(mesure).toUpperCase();
+        const maxValue = 100;
+        const text = `Please enter: ${mesure} (Format: number not more than ${maxValue}).`
+        let check = false;
+        let userInput = '';
+        do{
+            userInput = prompt(text);
+            check = !isNaN(+userInput) && +userInput <= maxValue && +userInput > 0 && userInput !== null;
+            if(!check && userInput !== null){
+                alert(`Try again. Remember, only positive number not more than ${maxValue}!`);
+            }else if(userInput === null){
+                alert('Sorry, you can not cancel until you enter a valid value.');
+            }
+        }while(!check); 
+        return userInput;   
+    }
+    // End Functions
+}
+// --------------------------------
 
