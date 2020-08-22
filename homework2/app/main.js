485ededbbcc5831954311673dbc5f275353d6ff1
denys-bobrovnykov <62986552+denys-bobrovnykov@users.noreply.github.com>
2020-08-22 23:47:58 +0300

    HW2.Task4 first commit.

--- homework2/app/main.js
+++ homework2/app/main.js
@@ -88,6 +88,8 @@ function taskTwo(){
     }
     // End Functions
 }
+// Qs:
+//     - How to sort Object data?
 // --------------------------------
 
 
@@ -126,3 +128,169 @@ function taskThree(){
 
 
 // --------- Task 4 ---------------
+
+// Minitask 1:
+
+// Запросить у пользователя его возраст и определить, кем он является: 
+// ребенком (0–12), 
+// подростком (12–18), 
+// взрослым (18-60), 
+// пенсионером (60– ...).
+function age(){
+    const age = +prompt('What is your age?');
+    switch(true){
+        case age > 0 && age < 12: {
+            alert('You are a child, sir.');
+            break;
+        }
+        case age >= 12 && age < 18: {
+            alert('You are a teenager.');
+            break;
+        }
+        case age >= 18 && age <60: {
+            alert('Congrats, you are an adult!');
+            break;
+        }
+        case age >= 60 && age <= 117: {
+            alert('You are probably a retired senior citizen.')
+            break;
+        }
+        case age > 117: {
+            alert('You are the older than the oldest living person in 2020.');
+            break;
+        }
+        default:
+            alert('Your age is off limits for me.');
+    }    
+}
+
+//Minitask 2:
+
+// Запросить у пользователя число от 0 до 9 
+// и вывести ему спецсимвол, который расположен на этой клавише (1–!, 2–@, 3–# и т. д).
+
+function symbol(){
+    const key = prompt('Press one number key from 0 to 9.');
+    switch(+key){
+        case 0: {
+            alert(`${key} - )`);
+            break;
+        }
+        case 1: {
+            alert(`${key} - !`);
+            break;
+        }
+        case 2: {
+            alert(`${key} - @`);
+            break;
+        }
+        case 3: {
+            alert(`${key} - #`);
+            break;
+        }
+        case 4: {
+            alert(`${key} - $`);
+            break;
+        }
+        case 5: {
+            alert(`${key} - %`);
+            break;
+        }
+        case 6: {
+            alert(`${key} - ^`);
+            break;
+        }
+        case 7: {
+            alert(`${key} - &`);
+            break;
+        }
+        case 8: {
+            alert(`${key} - *`);
+            break;
+        }
+        case 9: {
+            alert(`${key} - (`);
+            break;
+        }
+        default: {
+            alert('Wrong key');
+        }
+    }
+}
+
+
+// Minitask 3:
+
+// Запросить у пользователя трехзначное и число и 
+// проверить, есть ли в нем одинаковые цифры.
+
+function numCheck(){
+    const number = +prompt('Enter three digit number.');
+    const lengthCheck = number / 100 >= 1 && number / 100 <= 10;
+    let counter = 0;
+    if(!isNaN(number) && lengthCheck){
+        for(let i = 0; i < String(number).length; i++){
+            for(let j = i + 1; j < String(number).length; j++){
+                if (String(number)[i] == String(number)[j]){
+                    counter ++;
+                    break;
+                }
+            }
+        }
+        if(counter > 0){
+            alert('There are the same digits.');
+        }else{
+            alert('All digits are unique.');
+        }
+    }else{
+        alert('Sorry, wrong input.');
+    }
+}
+
+// Minitask 4
+
+// Запросить у пользователя год
+// и проверить, високосный он или нет. 
+// Високосный год либо кратен 400, либо кратен 4 и при этом не кратен 100.
+
+function leapYear(){
+    const year = +prompt('Enter year for check.');
+    if(!isNaN(year) && year >= 1){
+        const result = !(year % 100 && year % 400) || !(year % 4) ? alert('It is leap year.') : alert('It is ordinary year.');
+    }else{
+        alert('Wrong input.');
+    }
+}
+
+
+// Minitask 5
+
+// Запросить у пользователя пятиразрядное число 
+// и определить, является ли  оно палиндромом.
+
+function palindrome(){
+    const number = +prompt('Enter 5 digits number.');
+    const lengthCheck = number / (10 ** 4) >= 1 && number / (10 ** 4) <= 10;
+    let text = 'Yes, it is palindrome';
+    if(!isNaN(number) && lengthCheck){
+        const numStr = String(number);
+        for(let i = 0; i < 3; i++){
+            if(!(numStr[i] == numStr[4 - i])){
+                text = 'Not palindrome.';
+                break;
+            }
+        }
+        alert(text);
+    }else{
+        alert('Wrong input.');
+    }    
+}
+
+
+// Minitask 6
+
+// Написать конвертор валют. 
+// Пользователь вводит количество USD, 
+// выбирает, в какую валюту хочет перевести: EUR, UAH или GBP,
+//  и получает в ответ соответствующую сумму.
+
