151539dc8a1e213fa139fb6dca670120a4cbe4d8
denys-bobrovnykov <62986552+denys-bobrovnykov@users.noreply.github.com>
2020-09-03 14:42:57 +0300

    HW5.T2 states names change.

--- homework5/trafficlight/app/light.js
+++ homework5/trafficlight/app/light.js
@@ -3,39 +3,36 @@ document.addEventListener('DOMContentLoaded', () => {
     const redBulb = document.querySelector('.light.bulb.r');// RED
     const yellowBulb = document.querySelector('.light.bulb.y');// YELLOW
     const greenBulb = document.querySelector('.light.bulb.g');// GREEN
-    
-    yellowBulb.classList.add('yellow-light');// Iluminate YELLOW
-    let currentState = 'neutral-stop';// Set current state (neutral-stop)
+
+    yellowBulb.classList.toggle('yellow-light');// Iluminate YELLOW
+    let currentState = 'wait-stop';// Set current state (neutral-stop)
 
     // Listener for the whole traffic light element //
     document.querySelector('.device').addEventListener('click', (event) => {
-        if ( event.target.className != 'button' ) return; // if not button element --> NOT FIRE
-        currentState = switchLight(currentState, redBulb, yellowBulb, greenBulb); // else FIRE function
+        if ( event.target.className != 'button' ) return; // if not button element --> DO NOT FIRE
+        yellowBulb.classList.toggle('yellow-light');// Toogle YELLOW
+        currentState = switchLight(currentState, redBulb, greenBulb); // Toggle RED or GREEN
     });
 
 })
 // ______________________ Function _________________________________ //
-function switchLight(currentState, redBulb, yellowBulb, greenBulb) {
+function switchLight(currentState, redBulb, greenBulb) {
     switch(currentState) {
-        case 'neutral-stop': {
+        case 'wait-stop': {
             redBulb.classList.toggle('red-light');
-            yellowBulb.classList.toggle('yellow-light');
             return 'stop';
         }
         case 'stop': {
             redBulb.classList.toggle('red-light');
-            yellowBulb.classList.toggle('yellow-light');
-            return 'neutral-go';
+            return 'wait-go';
         }
-        case 'neutral-go': {
-            yellowBulb.classList.toggle('yellow-light');
+        case 'wait-go': {
             greenBulb.classList.toggle('green-light');
             return 'go';
         }
         case 'go': {
             greenBulb.classList.toggle('green-light');
-            yellowBulb.classList.toggle('yellow-light');
-            return 'neutral-stop';
+            return 'wait-stop';
         }
     }
 }
