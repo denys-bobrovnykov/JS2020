b658eb9c77174a359315e6a4f1ec8a03691a4fa2
denys-bobrovnykov <62986552+denys-bobrovnykov@users.noreply.github.com>
2020-09-06 20:37:21 +0300

    HW6. T2 added <br> after input.

--- homework6/check_input/resources/app/input.js
+++ homework6/check_input/resources/app/input.js
@@ -6,7 +6,6 @@ document.addEventListener('DOMContentLoaded', () => {
         let text = input.value;
         let char = text[text.length - 1];
         console.log(char);
-
         if ( Number.isFinite(+char) && event.which != 32 ) {
             input.value = text.replace(char, '');
         }
--- homework6/math/resources/app/math.js
+++ homework6/math/resources/app/math.js
@@ -1,3 +1,4 @@
+// -------------------- Main code ----------------------------------------- //
 document.addEventListener('DOMContentLoaded', () => {
     // Init elements variables
     const answer = document.querySelector('.answer-input');
@@ -8,25 +9,34 @@ document.addEventListener('DOMContentLoaded', () => {
     let result = displayQuestion();
     // Add event global listener
     document.addEventListener('click', (event) => {
-        response.innerHTML = '';
         if ( event.target == submitButton ) {// If click on SUBMIT
             if ( +answer.value != result ) {
+                answer.value = '';
                 result = displayQuestion();
                 response.innerHTML = 'Try again';
-                answer.value = '';
-                // console.log(result);
                 return;
             }
             response.innerHTML = 'Correct';
             answer.value = '';
-            result =displayQuestion();
+            result = displayQuestion();
         };
         if ( event.target == nextButton ) { // If click on NEXT
+            response.innerHTML = '';
             result = displayQuestion();
             answer.value = '';
         }
     })
 })
+// --------------- Functions --------------------------------------------- //
+function displayQuestion() {
+    const box = document.querySelector('.question-container');// Select question container
+    box.innerHTML = ''; // Clear question box
+    const operators = ['+', '-', '/', '*'];
+    const operator = operators[random(0, operators.length - 1)];
+    const [num1, num2] = operator != '*' ? [random(10,100), random(10,100)] : [random(2,30), random(2,30)];
+    box.innerHTML = `${num1} ${operator} ${num2} =`;
+    return evaluate(num1, num2, operator).toFixed(1);
+}
 function random(from, to) {
     return Math.floor(Math.random() * (to - from) + from + 1);
 }
@@ -37,13 +47,4 @@ function evaluate(num1, num2, operator) {
         case '-': return num1 - num2;
         case '/': return num1 / num2;
     }
-}
-function displayQuestion() {
-    const box = document.querySelector('.question-container');
-    box.innerHTML = ''; // Clear question box
-    const operators = ['+', '-', '/', '*'];
-    const operator = operators[random(0, operators.length - 1)];
-    const [num1, num2] = operator != '*' ? [random(10,100), random(10,100)] : [random(2,30), random(2,30)];
-    box.innerHTML = `${num1} ${operator} ${num2} =`;
-    return evaluate(num1, num2, operator).toFixed(1);
 }
