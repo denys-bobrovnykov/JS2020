66217a74c09bc3c4c96da0a16e55789c3cdcfd68
denys-bobrovnykov <62986552+denys-bobrovnykov@users.noreply.github.com>
2020-09-06 22:10:07 +0300

    HW6.T3 handler name add.

--- homework6/check_input/resources/app/input2.js
+++ homework6/check_input/resources/app/input2.js
@@ -1,8 +1,9 @@
 document.addEventListener('DOMContentLoaded', () => {
 
     const input = document.querySelector('.text-input');
-    input.addEventListener('keydown', (event) => {
-        if ( event.key >= '0' && event.key <= '9' ) event.preventDefault();
-        
-    });
+    input.addEventListener('keydown', check);
+
 })
+function check(event) {
+    if ( event.key >= '0' && event.key <= '9' ) event.preventDefault();
+}
