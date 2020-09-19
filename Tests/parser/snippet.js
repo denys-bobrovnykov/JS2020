eb5dc6e9b234806c451439541f7c307d9c73a8c1
denys-bobrovnykov <62986552+denys-bobrovnykov@users.noreply.github.com>
2020-09-19 16:11:40 +0300

    DB correction.

--- Tests/parser/snippet.js
+++ Tests/parser/snippet.js
@@ -3,4 +3,11 @@ const str = '1. I';
 
 const answ = str.match(/^\d\.\s.*[\p{L}I\d\\)I].?$/gu);
 
-console.log(answ);
+let tasksArr = [["ewrwe", "23:04"],
+["423523", "05:35"],
+["5235", "05:35"],
+["23523", "23:23"],
+["23532", "05:35"]];
+let i = tasksArr.indexOf(tasksArr.find(el => el[0] == "23532"));
+console.log(i);
+console.log(tasksArr);
