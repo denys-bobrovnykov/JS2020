5cd0ff5e10f54bc17b5197c5524b580b986ae0b5
denys-bobrovnykov <62986552+denys-bobrovnykov@users.noreply.github.com>
2020-09-22 14:49:36 +0300

    Load DB in controller.

--- Tests/app/index/index-control.js
+++ Tests/app/index/index-control.js
@@ -4,6 +4,7 @@ import IndexView from "./index-view.js"
 export default class IndexControl{
     
     constructor(){
+        this.loadDB();
         this.model = new IndexModel();
         this.view = new IndexView(this.selectChapter.bind(this), this.goClick.bind(this), this.checkBox.bind(this));
         if ( localStorage.getItem('location') == 'test') {
@@ -27,6 +28,12 @@ export default class IndexControl{
         window.location.href = './test.html';
     }
 
+    loadDB() {
+        loadQuestions();
+        loadAnswers();
+        loadChapters();
+    }
+
     selectChapter() {
         console.log('select');
         this.model.selectOptions(this.view.selectElement);
@@ -39,4 +46,56 @@ export default class IndexControl{
         localStorage.setItem('time_start', timeStart.toLocaleString());
     }
     
+}
+// -------- Functions ----------- //
+function loadQuestions(){
+    console.log()
+    if ( !localStorage.getItem('questions') ) {
+        console.group('Loading questions db');
+        fetch('./app/index/questions/QuestionsObj.json')
+        .then(res => res.json())
+        .then(data => {
+            storeData(data,'questions');
+            console.log('db loaded')
+        }, rej => document.body.innerHTML = "ERROR LOADING QUESTIONS DB")
+        .catch(err => console.error(err));
+    }
+}
+
+function loadAnswers(){
+    if ( !localStorage.getItem('answers') ) {
+        console.group('Loading answers db');
+        fetch('./app/index/answers/answersObj.json')
+        .then(res => res.json())
+        .then(data => {
+                storeData(data, 'answers');
+                console.log('db loaded')
+        }, rej => document.body.innerHTML = "ERROR LOADING ANSWERS DB")
+        .catch(err => console.error(err));
+    }
+} 
+
+function loadChapters(){
+    const chaptersRanges = {
+            1: [1,213],
+            2: [214,293],
+            3: [294,346],
+            4: [347,412],
+            5: [413, 481],
+            6: [482, 603],
+            7: [604, 702],
+            8: [703, 886],
+            9: [887, 1002],
+            10: [1003, 1092],
+            11: [1093, 1239],
+            12: [1240, 1469],
+            13: [1470, 1508],
+            14: [1509, 1661],
+            15: [1662, 1706]
+    }   
+    localStorage.setItem('chaptersRanges', JSON.stringify(chaptersRanges));
+}
+
+function storeData(data, name) {
+    localStorage.setItem(name, JSON.stringify(data));
 }
